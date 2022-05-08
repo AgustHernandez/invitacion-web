8c99b6a982c7239d5d88c07f2931346784f7f517
Emiliano Corsaro <18041059+EmiCorsaro@users.noreply.github.com>
2022-05-08 12:49:58 -0300

    Conexión con firestore + env variables

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,16 +1,12 @@
-import { initializeApp } from "firebase/app";
-
+//import {fb ,initializeApp } from "firebase/app";
+import fb from 'firebase/compat/app';
 const firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    databaseURL: process.env.DATABASE_URL,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID
+    apiKey: process.env.REACT_APP_FIRESTORE_API_KEY,
+    authDomain: process.env.REACT_APP_FIRESTORE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIRESTORE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIRESTORE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIRESTORE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIRESTORE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIRESTORE_APP_ID
   };
-
-const app = initializeApp(firebaseConfig);
-export default function getFirestoreApp() {
-    return app
-} 
+export const firebase = fb.initializeApp(firebaseConfig);
