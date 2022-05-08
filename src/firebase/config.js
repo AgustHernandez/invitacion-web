c56f611826639a49941e0237193b5578e86055a1
Emiliano Corsaro <18041059+EmiCorsaro@users.noreply.github.com>
2022-05-08 18:29:41 -0300

    Eliminando codigo comentado

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,14 +1,5 @@
 //import {fb ,initializeApp } from "firebase/app";
 import fb from 'firebase/compat/app';
-/*const firebaseConfig = {
-  apiKey: "AIzaSyDQ59jhXccy3A0PvbcYN64S09Mmz9zZVII",
-  authDomain: "invitacion-jazmin.firebaseapp.com",
-  databaseURL: "https://invitacion-jazmin-default-rtdb.firebaseio.com",
-  projectId: "invitacion-jazmin",
-  storageBucket: "invitacion-jazmin.appspot.com",
-  messagingSenderId: "826639281310",
-  appId: "1:826639281310:web:08ed2f17ee17a50d5fdf8f"
-};*/
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIRESTORE_API_KEY,
     authDomain: process.env.REACT_APP_FIRESTORE_AUTH_DOMAIN,
