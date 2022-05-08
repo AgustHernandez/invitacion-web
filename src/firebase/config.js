3b1f5899c9c3282b068a27d0537191f48b469993
Emiliano Corsaro <18041059+EmiCorsaro@users.noreply.github.com>
2022-05-08 12:53:27 -0300

    prueba env variables

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,12 @@
 //import {fb ,initializeApp } from "firebase/app";
 import fb from 'firebase/compat/app';
+console.log(process.env.REACT_APP_FIRESTORE_API_KEY)
+console.log(process.env.REACT_APP_FIRESTORE_AUTH_DOMAIN)
+console.log(process.env.REACT_APP_FIRESTORE_DATABASE_URL)
+console.log(process.env.REACT_APP_FIRESTORE_PROJECT_ID)
+console.log(process.env.REACT_APP_FIRESTORE_STORAGE_BUCKET)
+console.log(process.env.REACT_APP_FIRESTORE_MESSAGING_SENDER_ID)
+console.log(process.env.REACT_APP_FIRESTORE_APP_ID)
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIRESTORE_API_KEY,
     authDomain: process.env.REACT_APP_FIRESTORE_AUTH_DOMAIN,
