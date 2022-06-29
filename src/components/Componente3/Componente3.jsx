1cbaf95699fb1b98d5bf30c57cb33b7d1f667f9f
Agustina Hernandez <93223681+AgustHernandez@users.noreply.github.com>
2022-06-29 11:37:57 -0300

    Update Componente3.jsx

--- src/components/Componente3/Componente3.jsx
+++ src/components/Componente3/Componente3.jsx
@@ -12,7 +12,7 @@ function Componente3() {
             <h5 className='textoPlaylist'>Ayudame a hacer la playlist de mi fiesta con las canciones que creas infaltables para bailar</h5>
         </div>
         <div className='sectionBotonSpotify'>
-          <a href='https://open.spotify.com/playlist/503lUO38JjWqrzD4uibwRW?si=gy0BIIe8RLG2divTgkRoCA' target='_blank' rel="noreferrer" >
+          <a href='https://open.spotify.com/playlist/503lUO38JjWqrzD4uibwRW?si=db0ee85b4d404e91&pt=961003b4664d1e032f906bf05310deed' target='_blank' rel="noreferrer" >
             <button className='botonSpotify'> Spotify </button>
           </a>
         </div>
